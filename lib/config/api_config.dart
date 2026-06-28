4518cebc0c2d683b2795b3941bb751debd647fc2
AhmedMagdi90 <AhmedMagdi90@users.noreply.github.com>
2026-06-28 16:50:04 +0300

    Point mobile app to local network backend

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,5 +1,5 @@
 class ApiConfig {
-  static const String baseUrl = 'http://127.0.0.1:8000';
+  static const String baseUrl = 'http://192.168.1.47:8000';
 
   static String absoluteUrl(String path) {
     if (path.isEmpty) {
